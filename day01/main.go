4e4f8855b0d6e174e5cf02f64912ecf96c2b8449
Manuel Garcia F. <mgarciafrigo@gmail.com>
2024-12-01 12:34:04 -0300

    day 1

---
+++ day01/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	l, r := parseInput(os.Args[1])
+	fmt.Println("Part 1: ", solvePart1(l, r))
+	fmt.Println("Part 2: ", solvePart2(l, r))
+}
+
+func solvePart1(l, r []int) int {
+	slices.Sort(l)
+	slices.Sort(r)
+	if len(l) != len(r) {
+		panic("invalid input")
+	}
+	sum := 0
+	for i := range l {
+		sum += abs(l[i] - r[i])
+	}
+
+	return sum
+}
+
+func solvePart2(l, r []int) int {
+	occurrences := make(map[int]int, len(r))
+	for _, v := range r {
+		occurrences[v] += 1
+	}
+
+	sum := 0
+	for _, v := range l {
+		sum += v * occurrences[v]
+	}
+
+	return sum
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func parseInput(path string) ([]int, []int) {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	left := make([]int, 0)
+	right := make([]int, 0)
+
+	for scanner.Scan() {
+		ints := strings.Split(scanner.Text(), "   ")
+		l, err := strconv.Atoi(ints[0])
+		if err != nil {
+			panic(err)
+		}
+		r, err := strconv.Atoi(ints[1])
+		if err != nil {
+			panic(err)
+		}
+		left = append(left, l)
+		right = append(right, r)
+	}
+
+	return left, right
+}
